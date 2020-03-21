3ff909e97cf812974ddf3746b6af6d16effd8d89
ardakazanci <ardakazanci@anadolu.edu.tr>
2020-03-21 16:28:43 +0300

    Initial commit

---
+++ app/src/main/java/com/ardakazanci/androidmvvmsampleproject/utils/Status.kt
@@ -0,0 +1,10 @@
+package com.ardakazanci.androidmvvmsampleproject.utils
+
+/**
+ * Ağ Çağrısının geçerli durumunu UI Katmanına iletmekten sorumlu olacak bir enum sınıfı.
+ */
+enum class Status {
+
+
+
+}
